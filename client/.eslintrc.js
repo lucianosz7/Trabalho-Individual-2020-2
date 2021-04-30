0799aa08f73bab9ed636e382d5b71840d74cf90d
Luciano Santos <luciano_z7@hotmail.com>
2021-04-29 22:23:05 -0300

    Adding lint rule

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -54,6 +54,7 @@ module.exports = {
   // add your custom rules here
   rules: {
     'prefer-promise-reject-errors': 'off',
+    'typescript/no-var-requires': 'on',
 
 
     // allow debugger during development only
