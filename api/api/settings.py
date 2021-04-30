28ff9e9c3d193a52a48b3a7be5991901787c7f49
Luciano Santos <luciano_z7@hotmail.com>
2021-04-30 14:03:56 -0300

    Fixing condition

--- api/api/settings.py
+++ api/api/settings.py
@@ -86,7 +86,7 @@ if os.getenv('API_ENV') == 'test':
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-else if os.getenv('API_ENV') == 'production':
+elif os.getenv('API_ENV') == 'production':
     DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
