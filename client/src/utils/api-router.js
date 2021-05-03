69f3ac549d579bfd73d7e546f890c3600e9f7773
Luciano Santos <luciano_z7@hotmail.com>
2021-05-03 11:30:30 -0300

    Testing some files

--- client/src/utils/api-router.js
+++ client/src/utils/api-router.js
@@ -1,2 +1,2 @@
 export const API_PATH =  process.env.NODE_ENV === 'production' ? 'https://api-gces.herokuapp.com/' : 'http://localhost:8000/';
-console.log('aaaa', API_PATH)
+console.log('aaaAPI_PATH',  process.env.VUE_APP_API_URL)
